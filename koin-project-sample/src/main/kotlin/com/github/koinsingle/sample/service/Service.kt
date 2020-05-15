23ee27ee851ddbbffbcd2dbc4a83544602f2c6ac
Flavio Andrade Machado <flaviochess@gmail.com>
2020-05-15 02:18:47 -0300

    prevent empty round times

--- koin-project-sample/src/main/kotlin/com/github/koinsingle/sample/service/Service.kt
+++ koin-project-sample/src/main/kotlin/com/github/koinsingle/sample/service/Service.kt
@@ -1,10 +1,8 @@
 package com.github.koinsingle.sample.service
 
-import com.github.koinsingle.annotation.Single
 import com.github.koinsingle.sample.dao.Dao
 import com.github.koinsingle.sample.dao.OtherDao
 
-@Single
 class Service(
         private val dao: Dao,
         private val otherDao: OtherDao
