64c0703a24b5eb910d2bcb45b7079753d4bf86ab
Flavio Andrade Machado <flaviochess@gmail.com>
2020-05-24 02:11:50 -0300

    generate single binding interface

--- koin-project-sample/src/main/kotlin/com/github/koinsingle/sample/controller/Controller.kt
+++ koin-project-sample/src/main/kotlin/com/github/koinsingle/sample/controller/Controller.kt
@@ -2,11 +2,11 @@ package com.github.koinsingle.sample.controller
 
 import com.github.koinsingle.annotation.Single
 import com.github.koinsingle.sample.service.OtherService
-import com.github.koinsingle.sample.service.Service
+import com.github.koinsingle.sample.service.interfaces.Serviceable
 
 @Single
 class Controller(
-        private val service: Service,
+        private val service: Serviceable,
         private val otherService: OtherService
 ) {
 
